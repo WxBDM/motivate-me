05a3dac09ce2c5c3cd25231a2359b79fca1c4e84
Brandon M <bdmolyne@gmail.com>
2023-03-05 23:11:30 -0500

    changed setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='motivate-me',
-    version='0.1.0-alpha.1',
+    version='0.1.0',
     author='Brandon Molyneaux',
     author_email='brandon@learningcodeonline.com',
     description='A package leveraging ChatGPT to print out motivational quotes and one-liners.',
